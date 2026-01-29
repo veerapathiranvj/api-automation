2702b8d49e3a0faa38b72b6f7cff4590a92f51db
veerapathiranvj <veerapathiran4057@gmail.com>
2026-01-29 17:59:25 +0530

    updated

--- tests/practice1.spec.js
+++ tests/practice1.spec.js
@@ -1,7 +1,9 @@
 const{test,expect}=require('@playwright/test');
 test("put request",async({request})=>{
     const response=await request.put('https://jsonplaceholder.typicode.com/posts/1');
-    expect(response.status()).toBe(200);
+    expect(response.status()).toBe(200); 
+
+    console.log(await response.json());
 
 
 
