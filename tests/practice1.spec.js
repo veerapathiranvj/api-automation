478d9c42f6d79b66eefa97a159134bd1c7de0466
veerapathiranvj <veerapathiran4057@gmail.com>
2026-01-29 20:00:43 +0530

    correction panniyachu bro

--- tests/practice1.spec.js
+++ tests/practice1.spec.js
@@ -1,10 +1,21 @@
-const{test,expect}=require('@playwright/test');
-test("put request",async({request})=>{
-    const response=await request.put('https://jsonplaceholder.typicode.com/posts/1');
-    expect(response.status()).toBe(200); 
+const { test, expect } = require('@playwright/test');
+test("put request", async ({ request }) => {
+    const response = await request.put('https://jsonplaceholder.typicode.com/posts/1', {
+        data: {
+            id: 1,
+            title: "api put updated",
+            body: "updated body",
+        }
 
+
+    });
+    expect(response.status()).toBe(200);
     console.log(await response.json());
-    console.log("result:",response.json());
+    console.log("result:", await response.json());
+    const restdata = await response.json();
+    expect(restdata.id).toBe(1);
+    expect(restdata.title).toBe("api put updated");
+    expect(restdata.body).toBe("updated body");
 
 
 
