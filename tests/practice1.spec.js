6ccdb4081545aa896f5f7caf210d1237d1623e82
veerapathiranvj <veerapathiran4057@gmail.com>
2026-01-29 17:45:21 +0530

    created

---
+++ tests/practice1.spec.js
@@ -0,0 +1,8 @@
+const{test,expect}=require('@playwright/test');
+test("put request",async({request})=>{
+    const response=await request.put('https://jsonplaceholder.typicode.com/posts/1');
+    expect(response.status()).toBe(200);
+
+
+
+})
