672f08205a3142088110b2f708d85829395abf92
veerapathiranvj <veerapathiran4057@gmail.com>
2026-01-30 10:45:39 +0530

    task completed

--- tests/practice1.spec.js
+++ tests/practice1.spec.js
@@ -17,6 +17,25 @@ test("put request", async ({ request }) => {
     expect(restdata.title).toBe("api put updated");
     expect(restdata.body).toBe("updated body");
 
-
-
 })
+
+test('patch request', async ({ request }) => {
+    const patchrequest = await request.patch('https://jsonplaceholder.typicode.com/posts/1', {
+        data: {
+            name: "updated name"
+        }
+    })
+    const patchdata = await patchrequest.json()
+    expect(patchrequest.status()).toBe(200);
+    expect(patchdata.name).toBe("updated name");
+    console.log("patchdata:", patchdata)
+})
+     test('patch get request', async ({ request }) => {
+    const patchrequest = await request.get('https://jsonplaceholder.typicode.com/posts/1');
+    const patchdata = await patchrequest.json(); 
+    expect(patchrequest.status()).toBe(200);
+    console.log('patchdata:',patchdata);  //crt ah check pannu 3 test kum different irrku 
+     })
+
+
+
